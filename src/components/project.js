9ffaa19cc13e1adbfc9a0aa18ed7849bee145775
GUNASRI <gunasri0905@gmail.com>
2024-06-01 08:02:27 +0530

    portfolio project

--- src/components/project.js
+++ src/components/project.js
@@ -13,22 +13,25 @@ export default function Project() {
             </div>
             <div className="w-full">
                 <div className='flex flex-col md:flex-row px-10 gap-5'>
-                    <div className='relative'>
-                        <img src={Project1} className='h-[250px] w-[400px]' alt='Project1' />
-                        <div className='project-desc'>
-                            <p className='text-center px-10 py-20'>User- friendly BMI calculator by using React.js</p>
-                        </div>
-                    </div>
+                    
                     <div className='relative'>
                         <img src={Project2} className='h-[250px] w-[400px]' alt='Project2' />
                         <div className='project-desc'>
-                            <p className='text-center px-10 py-20'>TODO list app by using HTML/CSS, Javascript</p>
+                            <p className='text-center px-10 py-20'>TODO list app by using HTML/CSS, Javascript <br/>
+                            <span><a href='https://gunasri3105.github.io/todo-app/'>Click Here</a></span> </p>
                         </div>
                     </div>
                     <div className='relative'>
                         <img src={Project3} className='h-[250px] w-[400px]' alt='Project3' />
                         <div className='project-desc'>
-                            <p className='text-center px-10 py-20'>Responsive E-Commerce website by using React.js</p>
+                            <p className='text-center px-10 py-20'>Responsive E-Commerce website by using React.js <br/>
+                            <span><a href='https://gunasri3105.github.io/e-commerce/'>Click Here</a></span></p>  
+                        </div>
+                    </div>
+                    <div className='relative'>
+                        <img src={Project1} className='h-[250px] w-[400px]' alt='Project1' />
+                        <div className='project-desc'>
+                            <p className='text-center px-10 py-20'>User- friendly BMI calculator by using React.js</p>
                         </div>
                     </div>
                 </div>
